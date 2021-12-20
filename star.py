58194dbe2a02217e959bb7c2243655da62ce0012
acasasaez <acasasae@myuax.com>
2021-12-20 13:15:48 +0100

    creamos el bucle que permite que se dibuje la estrella

--- star.py
+++ star.py
@@ -3,3 +3,8 @@ turtle.fillcolor("purple")
 turtle.beingfill()
 a = int(input("Introduzca el número de estrellas: "))
 c = 1
+while c != a:
+    turtle.forward(250)
+    turtle.right (180-(180/a))
+    if c == a +1:
+        break
